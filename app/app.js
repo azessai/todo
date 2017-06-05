1a732ae7a1b89729942e08d8d44d72725a3729f4
azessai <azeddine.essai@gmail.com>
2017-06-05 12:59:53 +0200

    init

--- app/app.js
+++ app/app.js
@@ -1,103 +1,124 @@
 (function() {
-    'use strict';
+	'use strict';
 
 	var app = angular.module('main', ['route']);
 
-    app.factory('Todos', function($http) {
- 
-        return [
-        { name: 'Master HTML/CSS/Javascript', completed: true, note: 'note A' },
-        { name: 'Learn AngularJS', completed: false, note: 'note B' },
-        { name: 'Build NodeJS backend', completed: false, note: 'note C' },
-        { name: 'Get started with ExpressJS', completed: false, note: 'note D' },
-        { name: 'Setup MongoDB database', completed: false, note: 'note E' },
-        { name: 'Be awesome!', completed: false, note: 'note F' },
-        ];
- 
-    });
-    
-    app.service("dataService", function ($http, $q){
-        var deferred = $q.defer();
-        $http.get('/app/get.json').then(function(response) {
-			    //console.log('response', response);
-                deferred.resolve(response);
+	app.factory('Todos', function($http) {
 
-        });
-        this.getdata = function (){
-            return deferred.promise;
-        };
-    });
-    
-    app.controller('TodoController', ['$scope', '$http', 'Todos', 'dataService', function($scope, $http, Todos, dataService) {
-        $scope.msg = 'NON';
+		return [{
+				name: 'Master HTML/CSS/Javascript',
+				completed: true,
+				note: 'note A'
+			},
+			{
+				name: 'Learn AngularJS',
+				completed: false,
+				note: 'note B'
+			},
+			{
+				name: 'Build NodeJS backend',
+				completed: false,
+				note: 'note C'
+			},
+			{
+				name: 'Get started with ExpressJS',
+				completed: false,
+				note: 'note D'
+			},
+			{
+				name: 'Setup MongoDB database',
+				completed: false,
+				note: 'note E'
+			},
+			{
+				name: 'Be awesome!',
+				completed: false,
+				note: 'note F'
+			},
+		];
 
-        $scope.valider = function(text) {
-            $scope.saveToPc($scope.todos, '/app/save.json');
-            $scope.msg = 'Données enregistrées';
+	});
+
+	app.service("dataService", function($http, $q) {
+		var deferred = $q.defer();
+		$http.get('/app/get.json').then(function(response) {
+			//console.log('response', response);
+			deferred.resolve(response);
+
+		});
+		this.getdata = function() {
+			return deferred.promise;
+		};
+	});
+
+	app.controller('TodoController', ['$scope', '$http', 'Todos', function($scope, $http, Todos) {
+		$scope.msg = 'NON';
+
+		$scope.valider = function(text) {
+			$scope.saveToPc($scope.todos, '/app/save.json');
+			$scope.msg = 'Données enregistrées';
+		};
 
 
-        };
-        
-        
-        
-$scope.saveToPc = function (data, filename) {
 
-  if (!data) {
-    console.error('No data');
-    return;
-  }
+		$scope.saveToPc = function(data, filename) {
 
-  if (!filename) {
-    filename = 'download.json';
-  }
+			if (!data) {
+				console.error('No data');
+				return;
+			}
 
-  if (typeof data === 'object') {
-    data = JSON.stringify(data, undefined, 2);
-  }
+			if (!filename) {
+				filename = 'download.json';
+			}
 
-  var blob = new Blob([data], {type: 'text/json'});
+			if (typeof data === 'object') {
+				data = JSON.stringify(data, undefined, 2);
+			}
 
-  // FOR IE:
+			var blob = new Blob([data], {
+				type: 'text/json'
+			});
 
-  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(blob, filename);
-  }
-  else{
-      var e = document.createEvent('MouseEvents'),
-          a = document.createElement('a');
+			// FOR IE:
 
-      a.download = filename;
-      a.href = window.URL.createObjectURL(blob);
-      a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
-      e.initEvent('click', true, false, window,
-          0, 0, 0, 0, 0, false, false, false, false, 0, null);
-      a.dispatchEvent(e);
-  }
-};
-        
-        
-        
-        if (true)
-        {
-            
-            $http.get('/app/get.json').then(function(response) {
-			     //console.log('response', response);
-                $scope.todos = response.data;
-            });
-            
-        } else {
-            $scope.todos = Todos;
-        }
+			if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+				window.navigator.msSaveOrOpenBlob(blob, filename);
+			} else {
+				var e = document.createEvent('MouseEvents'),
+					a = document.createElement('a');
 
-    }]);
+				a.download = filename;
+				a.href = window.URL.createObjectURL(blob);
+				a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+				e.initEvent('click', true, false, window,
+					0, 0, 0, 0, 0, false, false, false, false, 0, null);
+				a.dispatchEvent(e);
+			}
+		};
 
-    app.controller('TodoDetailController', ['$scope', '$routeParams', '$location', 'Todos', function ($scope, $routeParams, $location, Todos) {
-        console.log($routeParams.id);
-        $scope.todo = Todos[$routeParams.id];
-        $scope.retour = function (){
-            $location.path("/");
-        }
-        
-    }]);
+	}]);
+	app.config('TodoController', ['$scope', '$http', 'Todos', function($scope, $http, Todos) {
+		console.log('configuration of my app.');
+		var test = true;
+		if (test) {
+			$http.get('/app/get.json').then(function(response) {
+				console.log('response', response);
+				$scope.todos = response.data;
+			});
+
+
+		} else {
+			$scope.todos = Todos;
+		}
+	}]);
+	app.controller('TodoDetailController', ['$scope', '$routeParams', '$location', 'Todos', function($scope, $routeParams, $location, Todos) {
+		console.log($routeParams.id);
+		$scope.todo = Todos[$routeParams.id];
+		$scope.retour = function() {
+			$location.path("/");
+		}
+
+	}]);
 
 })();
